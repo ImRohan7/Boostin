830d072bdd7e71ed1e328c6062cd724a9a909768
pinakj <pinak.jalan@me.com>
2019-09-05 11:40:08 -0600

    Twitch integration works

---
+++ Friends With Benefits/Assets/TwitchChat.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.IO;
+
+public class TwitchChat : MonoBehaviour
+{
+    [SerializeField]
+    private string username;
+    [SerializeField]
+    private string password;
+    [SerializeField]
+    private string channelName;
+
+    private TcpClient twitchClient;
+    private StreamReader reader;
+    private StreamWriter writer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!twitchClient.Connected)
+        {
+            Connect();
+        }
+
+        //TO DO: Try and optimize
+        ReadChat();
+    }
+
+    private void Connect()
+    {
+        twitchClient = new TcpClient("irc.chat.twitch.tv", 6667);
+        reader = new StreamReader(twitchClient.GetStream());
+        writer = new StreamWriter(twitchClient.GetStream());
+
+        writer.WriteLine("PASS " + password);
+        writer.WriteLine("NICK " + username);
+        writer.WriteLine("USER " + username + " 8 * :" + username);
+        writer.WriteLine("JOIN #" + channelName);
+        writer.Flush();
+
+    }
+
+    private void ReadChat()
+    {
+        if (twitchClient.Available > 0)
+        {
+            var message = reader.ReadLine(); //Read in the current message
+            print(message);
+
+            if (message.Contains("PRIVMSG"))
+            {
+              
+            }
+        }
+    }
+}
