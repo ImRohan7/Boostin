22dc184c7642fdc9000512b2d42099c1d42c07a9
Pinak Jalan <pinak.jalan@me.com>
2019-09-09 14:08:52 -0600

    added bitch manager

---
+++ Friends With Benefits/Assets/BitchManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitchManager : MonoBehaviour
+{
+    [Header("Number of times each bitch has won")]
+    public int BWins;
+    public int TWins;
+    public int CWins;
+    public int HWins;
+
+    public int[] bitchArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        BWins = 0;
+        TWins = 0;
+        CWins = 0;
+        HWins = 0;
+
+        bitchArray = new int[] { BWins, TWins, CWins, HWins };
+    }
+
+    public int[] increaseBitchWin(ref int bitchWhichWon)
+    {
+        bitchWhichWon++;
+        bitchArray = new int[] { BWins, TWins, CWins, HWins };
+
+        return bitchArray;
+    }
+}
