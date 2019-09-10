79ef4540e8d159c3b52c1e7911ab65b0e27e193d
Rohan Patel <rohanpatel1899@gmail.com>
2019-09-09 21:39:31 -0600

    cleaning

--- Friends With Benefits/Assets/rotator.cs
+++ Friends With Benefits/Assets/rotator.cs
@@ -7,22 +7,10 @@ public class rotator : MonoBehaviour
     [SerializeField]
     private float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         float current = transform.eulerAngles.z;
         transform.eulerAngles = new Vector3(0, 0, current + speed);
-        //Quaternion rotation = transform.rotation;
-        //Quaternion k = rotation;
-        //k.Set(k.x, k.y, k.z + Time.deltaTime * speed, k.w);
-        //transform.rotation = k;
-        //Debug.Log(k.z);
-        // transform.Rotate(Vector3.left * Time.deltaTime * speed);
     }
 }
