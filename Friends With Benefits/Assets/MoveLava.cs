4a5e7ae3143495351b4b396320151b458f367fd0
pinakj <pinak.jalan@me.com>
2019-09-09 19:55:28 -0600

    more changes

--- Friends With Benefits/Assets/MoveLava.cs
+++ Friends With Benefits/Assets/MoveLava.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class MoveLava : MonoBehaviour
 {
+    [SerializeField]
+    private Vector3 position;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 v3Pos = Camera.main.ViewportToWorldPoint(new Vector3(-0.25f, 0.5f, 10.0f));
-        gameObject.transform.position = v3Pos;
+        //Vector3 v3Pos = Camera.main.ViewportToWorldPoint(new Vector3(-0.30f, 0.5f, 10.0f));
+       Vector3 pos = Camera.main.ViewportToWorldPoint(position);
+       gameObject.transform.position = pos;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.right * 1f * Time.deltaTime;
     }
 }
